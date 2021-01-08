b1af9f54595b6be14ea44fb6606641bd8f104743
GW6451 <gw6451@naver.com>
2021-01-09 02:00:46 +0900

    유경 컴에서 lombok적용이 안되서 getter/setter 어노테이션 사용 불가...

--- src/main/java/com/kosmo/shoong/service/pack/PackScheduleEventsDTO.java
+++ src/main/java/com/kosmo/shoong/service/pack/PackScheduleEventsDTO.java
@@ -12,5 +12,29 @@ public class PackScheduleEventsDTO {
 	private String title;
 	private Date start;
 	private Date end;
-
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
 }
