d97513aff50db9ee8c94b1b8bc5e332d7efa2808
suggom <wkungh93@gmail.com>
2021-01-21 14:12:07 +0900

    팩 멤버 인서트 수정

--- src/main/resources/com/kosmo/shoong/member/mybatis/memberInsert.sql
+++ src/main/resources/com/kosmo/shoong/member/mybatis/memberInsert.sql
@@ -1,31 +1,33 @@
-INSERT INTO member_ VALUES('KIM@naver.com','1234','김길동','남성',28,'01063408615',sysdate,sysdate,132);
+/* =================================================팩장에 해당하는 유저 생성 ===========================================================*/
+INSERT INTO member_ VALUES('shoong1000@naver.com','1234','장동건','남성',25,'01000001000',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1100@naver.com','1234','한지민','여성',30,'010000001100',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1200@naver.com','1234','이동건','남성',45,'01000001200',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1300@naver.com','1234','이나영','여성',33,'01000001300',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1400@naver.com','1234','김태희','여성',41,'01000001400',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1500@naver.com','1234','비','남성',35,'01000001500',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1600@naver.com','1234','신세경','여성',26,'01000001600',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1700@naver.com','1234','한효주','여성',29,'01000001700',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1800@naver.com','1234','아이유','여성',32,'01000001800',sysdate,sysdate,132);
+INSERT INTO member_ VALUES('shoong1900@naver.com','1234','고수','남성',42,'01000001900',sysdate,sysdate,132);
 
-INSERT INTO member_ VALUES('NA@gmail.com','1234','나길동','여성',29,'01056842684',sysdate,sysdate,132);
+INSERT INTO verify VALUES('shoong1000@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1100@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1200@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1300@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1400@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1500@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1600@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1700@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1800@naver.com', 'Y');
+INSERT INTO verify VALUES('shoong1900@naver.com', 'Y');
 
-INSERT INTO member_ VALUES('LIM@daum.net','1234','임유리','여성',26,'01048649756',sysdate,sysdate,132);
-
-INSERT INTO member_ VALUES('LA@nate.com','1234','라유경','여성',28,'01085612364',sysdate,sysdate,132);
-
-INSERT INTO member_ VALUES('HAHA@naver.com','1234','하유경','여성',20,'01075963142',sysdate,sysdate,132);
-
-
-INSERT INTO verify VALUES('KIM@naver.com', 'Y');
-
-INSERT INTO verify VALUES('NA@gmail.com', 'Y');
-
-INSERT INTO verify VALUES('LIM@daum.net', 'Y');
-
-INSERT INTO verify VALUES('LA@nate.com', 'N');
-
-INSERT INTO verify VALUES('HAHA@naver.com', 'N');
-
-
-INSERT INTO useraddr VALUES('KIM@naver.com', '서울시 구로구',37.488004666666676,126.85213716666668);
-
-INSERT INTO useraddr VALUES('NA@gmail.com', '서울시 금천구',37.47351705665664,126.89545164566394);
-
-INSERT INTO useraddr VALUES('LIM@daum.net', '서울시 영등포구',37.521388135005814,126.90696856433206);
-
-INSERT INTO useraddr VALUES('LA@nate.com', '경기도 광명시',37.482706679761,126.85998213398534);
-
-INSERT INTO useraddr VALUES('HAHA@naver.com', '서울시 강서구',37.55160252000908,126.84953247711086);
+INSERT INTO useraddr VALUES('shoong1000@naver.com', '서울시 구로구',37.488004666666676,126.85213716666668);
+INSERT INTO useraddr VALUES('shoong1100@naver.com', '경기도 용인시',37.324501882069015,127.25959003827327);
+INSERT INTO useraddr VALUES('shoong1200@naver.com', '경기도 성남시',37.40953083324051,127.07590473712015);
+INSERT INTO useraddr VALUES('shoong1300@naver.com', '경기도 용인시',37.25030499290375,127.14752182834968);
+INSERT INTO useraddr VALUES('shoong1400@naver.com', '경기도 광주시',37.31789377151831,127.36065024001839);
+INSERT INTO useraddr VALUES('shoong1500@naver.com', '경기도 광주시',37.32904468961533,127.28993505327924);
+INSERT INTO useraddr VALUES('shoong1600@naver.com', '경기도 성남시',37.45073082410645,127.11391951067414);
+INSERT INTO useraddr VALUES('shoong1700@naver.com', '경기도 성남시',37.43073658170476,127.19126149235792);
+INSERT INTO useraddr VALUES('shoong1800@naver.com', '경기도 성남시',37.399067052411404,127.16010054234708);
+INSERT INTO useraddr VALUES('shoong1900@naver.com', '경기도 용인시',37.289404999999995,127.1252265);
