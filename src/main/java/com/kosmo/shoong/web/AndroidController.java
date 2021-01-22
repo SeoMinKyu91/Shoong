6e9b06bb1cf8c3f47ce61c40bb4476c6a0c3e920
suggom <wkungh93@gmail.com>
2021-01-22 17:10:40 +0900

    안드로이드 컨트롤러 생성

---
+++ src/main/java/com/kosmo/shoong/web/AndroidController.java
@@ -0,0 +1,11 @@
+package com.kosmo.shoong.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AndroidController {
+	//안드로이드 컨트롤러
+	//안드 로그인
+	//안드 경로 전송
+	//안드 경로 저장
+}
