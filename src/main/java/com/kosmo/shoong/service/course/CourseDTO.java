3b7ddc2522a07a7a80c15c44f17d8e2ed8ccd4ff
GW6451 <gw6451@naver.com>
2021-02-03 19:16:55 +0900

    lombok 해제 후 직접 코딩

--- src/main/java/com/kosmo/shoong/service/course/CourseDTO.java
+++ src/main/java/com/kosmo/shoong/service/course/CourseDTO.java
@@ -18,9 +18,6 @@ import lombok.ToString;
  COURSE_TAG                                         NVARCHAR2(50)
  COURSE_REGION                                      NVARCHAR2(50) 
  */
-@Setter
-@Getter
-@ToString
 public class CourseDTO {
 
 	private String courseId;
@@ -34,4 +31,75 @@ public class CourseDTO {
 	
 	private String userId;
 	private String courseCateId;
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getCourseIntro() {
+		return courseIntro;
+	}
+	public void setCourseIntro(String courseIntro) {
+		this.courseIntro = courseIntro;
+	}
+	public String getCourseTime() {
+		return courseTime;
+	}
+	public void setCourseTime(String courseTime) {
+		this.courseTime = courseTime;
+	}
+	public String getCourseLength() {
+		return courseLength;
+	}
+	public void setCourseLength(String courseLength) {
+		this.courseLength = courseLength;
+	}
+	public Date getCourseRegiDate() {
+		return courseRegiDate;
+	}
+	public void setCourseRegiDate(Date courseRegiDate) {
+		this.courseRegiDate = courseRegiDate;
+	}
+	public String getCourseTag() {
+		return courseTag;
+	}
+	public void setCourseTag(String courseTag) {
+		this.courseTag = courseTag;
+	}
+	public String getCourseRegion() {
+		return courseRegion;
+	}
+	public void setCourseRegion(String courseRegion) {
+		this.courseRegion = courseRegion;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCourseCateId() {
+		return courseCateId;
+	}
+	public void setCourseCateId(String courseCateId) {
+		this.courseCateId = courseCateId;
+	}
+	@Override
+	public String toString() {
+		return "CourseDTO [courseId=" + courseId + ", courseName=" + courseName + ", courseIntro=" + courseIntro
+				+ ", courseTime=" + courseTime + ", courseLength=" + courseLength + ", courseRegiDate=" + courseRegiDate
+				+ ", courseTag=" + courseTag + ", courseRegion=" + courseRegion + ", userId=" + userId
+				+ ", courseCateId=" + courseCateId + "]";
+	}
+	
+	
+	
+	
 }
