832906f71eaff545ef45505bfba84326c87c7e81
suggom <wkungh93@gmail.com>
2021-02-04 15:42:34 +0900

    badge insert & user_inter_course add

--- src/main/java/com/kosmo/shoong/service/impl/badge/BadgeDAO.java
+++ src/main/java/com/kosmo/shoong/service/impl/badge/BadgeDAO.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kosmo.shoong.service.badge.BadgeDTO;
 import com.kosmo.shoong.service.badge.BadgeService;
 
 @Repository
@@ -19,11 +20,11 @@ public class BadgeDAO implements BadgeService{
 
 	@Override
 	public int insertBadge(Map map) {
-		List<Integer> rList = sqlMapper.selectList("whereIsMyBadge", map);
+		List<BadgeDTO> badgeList = sqlMapper.selectList("whereIsMyBadge", map);
 		
 		//for(int result:rList) System.out.println(result);
 		//map.put("badgeIdList", rList);
-		map.put("badgeId", rList.get(0)-1);
+		map.put("badgeList", badgeList);
 		Set keys = map.keySet();
 		for(Object key:keys) System.out.println(key+":"+map.get(key));
 		return sqlMapper.insert("insertMileageBadge", map);
