47cf2be35eef4fac5c5cc62edf50ea606e647e2d
baegunggi <baegunggi@gmail.com>
2021-02-07 15:13:18 +0900

    test_master merge전푸쉬

--- src/main/resources/com/kosmo/shoong/badge/mybatis/badgeinsert.sql
+++ src/main/resources/com/kosmo/shoong/badge/mybatis/badgeinsert.sql
@@ -5,8 +5,8 @@
  BEDGE_CATEGORY                                     VARCHAR2(30)
  BADGE_THUMBNAIL                                    NVARCHAR2(30)
  */
-INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'새싹','0','마일리지','starter_badge.jpg');
-INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'비기너','1','마일리지','beginner_badge.jpg');
-INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'걸음마','5','마일리지','first_step_badge.jpg');
-INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'동네 산책','10','마일리지','walk_badge.jpg');
-INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'동네 한바퀴','15','마일리지','circle_badge.jpg');
+INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'새싹','0','마일리지','shoongBadge1.png');
+INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'비기너','1','마일리지','shoongBadge2.png');
+INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'걸음마','5','마일리지','shoongBadge3.png');
+INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'동네 산책','10','마일리지','shoongBadge4.png');
+INSERT INTO BADGE VALUES(SEQ_BADGE.NEXTVAL,'동네 한바퀴','15','마일리지','shoongBadge5.png');
